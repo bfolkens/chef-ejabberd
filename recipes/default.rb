91d719c32af33f9e2fa0c23aecc3052042759b6d
Mike Nicholaides <mike.nicholaides@gmail.com>
2013-06-21 10:53:52 -0400

    Use erlang official deb package, rather than build from source

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,27 +1,30 @@
-package "ejabberd"
-
-%w{build-essential m4 libncurses5-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk libexpat1-dev libxml2-utils}.each do |pkg|
+%w{build-essential m4 libncurses5-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk libexpat1-dev libxml2-utils git-core}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-bash "compile ejabberd" do
-  code <<-EOH
-    mkdir -p /src/erlang
-    cd /src/erlang
-    wget http://www.erlang.org/download/otp_src_R16B.tar.gz
-    tar -xvzf otp_src_R16B.tar.gz
-    chmod -R 777 otp_src_R16B
-    cd otp_src_R16B
-    ./configure --prefix=/usr && make && make install
+remote_file "#{Chef::Config[:file_cache_path]}/esl-erlang_16.b-2~ubuntu~precise_amd64.deb" do
+  source "https://elearning.erlang-solutions.com/couchdb//rbingen_adapter//package_R16B_precise64_1361901944/esl-erlang_16.b-2~ubuntu~precise_amd64.deb"
+end
 
+dpkg_package "erlang" do
+  source "#{Chef::Config[:file_cache_path]}/esl-erlang_16.b-2~ubuntu~precise_amd64.deb"
+  action :create_if_missing
+end
+
+bash "install rebar" do
+  code <<-EOH
     cd ~/
     git clone git://github.com/rebar/rebar.git
     cd rebar/
     ./bootstrap
     cp rebar /usr/bin/
+  EOH
+end
 
+bash "compile ejabberd" do
+  code <<-EOH
     cd ~/
     git clone git://git.process-one.net/ejabberd/mainline.git ejabberd
     cd ejabberd
@@ -29,7 +32,11 @@ bash "compile ejabberd" do
     cd src
     ./configure --enable-odbc --prefix=/usr --enable-user=ejabberd --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib
     make install
+  EOH
+end
 
+bash "install ejabberd mysql" do
+  code <<-EOH
     cd ~/
     git clone https://github.com/processone/mysql
     cd mysql/
